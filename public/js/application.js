3a1071831e946362ba15a9878c7904f0a940b872
Chantal Emmanuel <cemmanuel1@gmail.com>
2013-07-04 15:05:08 -0700

    added inital API components

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,35 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	var myCenter=new google.maps.LatLng(37.792507, -122.406024);
+
+	function initialize()
+	{
+		var mapOptions = {
+			zoom: 8,
+			center: myCenter,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	var map=new google.maps.Map(document.getElementById('map-canvas'),
+			mapOptions);
+	
+
+	var marker=new google.maps.Marker({
+		position:myCenter,
+	});
+
+marker.setMap(map);
+}
+
+
+	function loadScript() {
+		var script = document.createElement('script');
+		script.type = 'text/javascript'
+  	script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+      'callback=initialize';
+  document.body.appendChild(script);
+}
+
+
+window.onload = loadScript;
 });
